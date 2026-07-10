a5959f9b24aa23e59da065f2840a987cc1cfded0
Jaydevlee <yousick5@gmail.com>
2026-07-10 09:33:19 +0900

    feat: appconfig

---
+++ Plc_Modbus/Config/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plc_Modbus.Config
+{
+    public class AppConfig
+    {
+        public DBSettings? dbSettings { get; set; }
+        public TagSetting? tagSetting { get; set; }
+    }
+}
