61d5d80dceae008ba4c717671236e109abf557df
Jaydevlee <yousick5@gmail.com>
2026-07-10 15:22:35 +0900

    Add: config-driven PLC mapping and DB repository setup
    - Load DB and tag settings from config.json
    - Map Coil and Holding data into PlcDBDto lists
    - Add DB connection creation logic
    - Add PLC repository insert structure
    - Wire PLC reader toward mapping flow

--- Plc_Modbus/data/DB_Conn.cs
+++ Plc_Modbus/data/DB_Conn.cs
@@ -1,5 +1,7 @@
-﻿using Npgsql;
+﻿using Microsoft.Win32.SafeHandles;
+using Npgsql;
 using Plc_Modbus.Config;
+using Plc_Modbus.Model;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,51 +12,41 @@ namespace Plc_Modbus.data
 {
     public class DB_Conn
     {
-        private string host;
-        private string port;
-        private string username;
-        private string password;
-        private string database;
-        private bool isConnected = false;
-
+        private readonly string? _connString = null;
+        
         private readonly AppConfig _appConfig = new();
-  
+
         public DB_Conn()
         {
             _appConfig = JsonSerializer.Deserialize<AppConfig>(File.ReadAllText("config.json"));
-            host = _appConfig.DBSettings.Host;
-            port = _appConfig.DBSettings.Port;
-            username = _appConfig.DBSettings.Username;
-            password = _appConfig.DBSettings.Password;
-            database = _appConfig.DBSettings.Database;
+            var db = _appConfig.DBSettings;
+            _connString = $"Host={db?.Host};" +
+                          $"Port={db?.Port};" +
+                          $"Username={db?.Username};" +
+                          $"Password={db?.Password};" +
+                          $"Database={db?.Database};";
         }
 
         public bool connectDB()
         {
-            string connString = $"Host={host};" +
-                                $"Port={port};" +
-                                $"Username={username};" +
-                                $"Password={password};" +
-                                $"Database={database}";
-            if (!isConnected)
+            try
             {
-                try
+                using (var conn = new NpgsqlConnection(_connString))
                 {
-                    using (var conn = new NpgsqlConnection(connString))
-                    {
-                        conn.Open();
-                        Debug.WriteLine("연결성공");
-                        return true;
-                    }
-                } 
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.Message);
-                    isConnected = false;
-                    return isConnected;
+                    conn.Open();
+                    Debug.WriteLine("연결성공");
+                    return true;
                 }
+            } 
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
             }
-            return isConnected;
+        }
+        public NpgsqlConnection CreateConn()
+        {
+            return new NpgsqlConnection(_connString);
         }
     }
 }
---
+++ Plc_Modbus/data/PLC_Repository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Npgsql;
+using Plc_Modbus.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Plc_Modbus.data
+{
+    public class PLC_Repository
+    {
+        private readonly DB_Conn _Conn = new DB_Conn();
+
+        public void InsertSensorData(List<PlcDBDto> readData)
+        {
+            using (var conn = _Conn.CreateConn())
+            {
+                try
+                {
+                    conn.Open();
+                    string sql = @"INSERT INTO sensor_data (time, equip_id, address, metric_name, metric_value)
+                            VALUES(NOW(), @Equip_id, @Address, @Metric_name, @Metric_value)";
+                    conn.Execute(sql, readData);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
