e50011cd850014d927e4fa7590803caf7c680979
Jaydevlee <yousick5@gmail.com>
2026-08-13 17:39:11 +0900

    add: 관계형테이블 추가

--- Script.sql
+++ Script.sql
@@ -19,25 +19,66 @@ SELECT create_hypertable(
 CREATE INDEX ix_metric_equip_name_time
     ON equipment_metric (equip_id, metric_name, time DESC);
 
-SELECT
-    column_name,
-    data_type,
-    is_nullable,
-    column_default
-FROM information_schema.columns
-WHERE table_schema = 'public'
-  AND table_name = 'equipment_metric'
-ORDER BY ordinal_position;
-
-
-select * from equipment_metric;
-
 CREATE TABLE IF NOT EXISTS product  (
-	id	                int       PRIMARY KEY,
+	product_id	        text    PRIMARY KEY,
 	name                text	  NOT NULL,
-	recipe_version      text      NOT NULL 
+	recipe_version      text      NOT NULL,
+	is_active			boolean	  NOT NULL DEFAULT TRUE 
 );
 
 CREATE TABLE IF NOT EXISTS equipment (
+	equip_id    text      PRIMARY KEY,
+	name		text	  NOT NULL,
+	location	text	  NOT NULL,
+	status      text      NOT  NULL,
+	is_active   boolean	  NOT NULL DEFAULT TRUE 
+);
 
-)
+CREATE TABLE IF NOT EXISTS work_order(
+	work_order_id     text    	PRIMARY KEY,
+	product_id 	      text 		NOT NULL,
+	target_quantity   int	 	NOT NULL,
+	status 			  text	 	NOT NULL,
+	planned_at		  timestamptz,
+	started_at		  timestamptz,
+	completed_at      timestamptz,
+	created_at        timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE INDEX IF NOT EXISTS ix_work_order_product_id
+	ON work_order(product_id);
+
+CREATE TABLE IF NOT EXISTS lot (
+	lot_id		   text			PRIMARY KEY,
+	work_order_id  text  		NOT NULL,
+	equip_id       text   		NOT NULL,
+	status		   text			NOT NULL,
+	start_at       timestamptz  NOT NULL DEFAULT NOW(),
+	end_at         timestamptz  
+);
+
+CREATE INDEX IF NOT EXISTS ix_lot_work_order_id
+    ON lot(work_order_id);
+CREATE INDEX IF NOT EXISTS ix_lot_equip_id
+    ON lot(equip_id);
+CREATE INDEX IF NOT EXISTS ix_lot_work_order_equip_id
+	ON lot(work_order_id, equip_id);
+
+CREATE TABLE IF NOT EXISTS production_result (
+	result_id	text		PRIMARY KEY,
+	lot_id      text		NOT NULL,
+	result      text		NOT NULL,
+	cycle_time  double PRECISION,
+	produced_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS ix_production_result_lot_id
+	ON production_result(lot_id);
+
+CREATE TABLE IF NOT EXISTS primary_sequence (
+	table_name	 text	 NOT NULL,
+	prefix 		 text	 NOT NULL,
+	years        text    DEFAULT TO_CHAR(CURRENT_DATE, 'YY'),
+	current_val	 bigint  NOT NULL DEFAULT 0,
+	CONSTRAINT pk_primary_seq PRIMARY KEY(table_name, years)
+);
