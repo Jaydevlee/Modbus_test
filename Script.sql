7ebcf4427bf455eea0ce83b001c18e4da23bd3c6
Jaydevlee <yousick5@gmail.com>
2026-08-13 10:48:04 +0900

    add: sql파일 추가

--- Script.sql
+++ Script.sql
@@ -10,11 +10,34 @@ CREATE TABLE equipment_metric (
     collected_at  timestamptz      NOT NULL DEFAULT now()
 );
 
-CREATE INDEX ix_metric_equip_name_time
-    ON equipment_metric (equip_id, metric_name, time DESC);
-
 SELECT create_hypertable(
     'equipment_metric',
     by_range('time'),
     if_not_exists => TRUE
 );
+
+CREATE INDEX ix_metric_equip_name_time
+    ON equipment_metric (equip_id, metric_name, time DESC);
+
+SELECT
+    column_name,
+    data_type,
+    is_nullable,
+    column_default
+FROM information_schema.columns
+WHERE table_schema = 'public'
+  AND table_name = 'equipment_metric'
+ORDER BY ordinal_position;
+
+
+select * from equipment_metric;
+
+CREATE TABLE IF NOT EXISTS product  (
+	id	                int       PRIMARY KEY,
+	name                text	  NOT NULL,
+	recipe_version      text      NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS equipment (
+
+)
