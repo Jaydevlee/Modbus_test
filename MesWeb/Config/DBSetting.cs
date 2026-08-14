883a67d1787eb08fcc71d6d0984e2bfb0c96067c
Jaydevlee <yousick5@gmail.com>
2026-08-14 15:07:16 +0900

    feat: MesWeb(Blazor)

---
+++ MesWeb/Config/DBSetting.cs
@@ -0,0 +1,8 @@
+public class DBSetting
+{
+    public string? Host { get; set; }
+    public string? Port { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Database { get; set; }
+}
