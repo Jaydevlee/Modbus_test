36b049dff973d1c9302f3ab8e0271103631c5652
Jaydevlee <yousick5@gmail.com>
2026-08-19 18:26:34 +0900

    feat: 페이징 처리 및 유효성 검사

--- Script.sql
+++ Script.sql
@@ -97,3 +97,14 @@ VALUES
 ON CONFLICT (table_name, years) DO NOTHING;
 SELECT * FROM PRODUCT;
 SHOW timezone;
+
+SELECT * FROM product;
+TRUNCATE TABLE product;
+
+UPDATE  product
+	SET is_active = false
+WHERE name = 'test3';
+
+UPDATE primary_sequence
+	SET current_val = 0
+WHERE table_name = 'product';
